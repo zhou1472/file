5041859b06f45d049460579bb31cfd61594c194b
zhou1472 <86612919+zhou1472@users.noreply.github.com>
2021-07-02 21:01:36 +0800

    Update MCMC.R

--- MCMC.R
+++ MCMC.R
@@ -2,18 +2,18 @@
 
 # MCsim=function(l=10,p=2,sd=0.05){
 #   par(mfrow=c(1,1))
-#   d1=rnorm(length,0,sd)
+#   d1=rnorm(l,0,sd)
 #   d2=d1+1
 #   d3=cumprod(d2)
-#   
-#   plot(d3,type="1",col=0,pch='o',
+# 
+#   plot(d3,type="l",col=0,pch='o',
 #       main="MC sim",
 #       ylab="CRP from 1",
 #       xlab="num of time units",
 #       lty=1,ylim=c(-1,3),
-#       xlim=c(1,length)
+#       xlim=c(1,l)
 #       )
-#   
+# 
 #   for(i in 0:(p)){
 #     d1=rnorm(l,0,sd)
 #     d2=d1+1
